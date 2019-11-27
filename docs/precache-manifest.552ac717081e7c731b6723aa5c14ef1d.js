6680fa45e63147f5aaaf046a9bc8f7b210239552
NookieGrey <gaguliyanugzar@gmail.com>
2019-11-27 19:03:56 +0400

    upload build

---
+++ docs/precache-manifest.552ac717081e7c731b6723aa5c14ef1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d70a23e9b4f0e19f5cc36f5c483aaf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98dfcca0aaab5cafa87c",
+    "url": "/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "74835c17536885a80431",
+    "url": "/static/css/main.c1835de2.chunk.css"
+  },
+  {
+    "revision": "98dfcca0aaab5cafa87c",
+    "url": "/static/js/2.072906d3.chunk.js"
+  },
+  {
+    "revision": "74835c17536885a80431",
+    "url": "/static/js/main.cf38aa3d.chunk.js"
+  },
+  {
+    "revision": "de4eb2caca3c26b7a13d",
+    "url": "/static/js/runtime-main.a8b3d859.js"
+  }
+]);
